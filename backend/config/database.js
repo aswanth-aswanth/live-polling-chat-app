c2440cbad012d3a4b358c033cb2db00ff1aa54ce
ASWANTH T <aswanthndl@gmail.com>
2024-09-03 18:17:48 +0530

    refactor(shared): Add atlas db

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
+import { configDotenv } from 'dotenv';
+configDotenv();
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      'mongodb://localhost:27017/polling_chat_app'
+      process.env.DB_URL || 'mongodb://localhost:27017/polling_chat_app'
     );
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
